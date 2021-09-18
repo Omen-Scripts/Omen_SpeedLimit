b50bac977ebe0304c97724cb26191240c0463e82
Joker12-Mods <55774965+Joker12-Mods@users.noreply.github.com>
2021-09-17 20:24:17 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Omen Scripts'
+
+ui_page "html/index.html"
+
+files {
+    "html/index.html",
+    "html/style.css",
+    "html/reset.css",
+    "html/listener.js"
+}
+
+client_scripts {
+    "client/main.lua",
+}
